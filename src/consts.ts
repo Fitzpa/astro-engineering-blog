9b96a32f9bd70423eed8674d3ff01582ac52f5d6
Louie Fitzpatrick <fitz4492@gmail.com>
2023-07-18 13:05:55 -0500

    updating constants

--- src/consts.ts
+++ src/consts.ts
@@ -1,8 +1,8 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const BRAND = "Acme";
-export const SITE_TITLE = "Astro Engineering Blog";
-export const SITE_URL = "https://astro-engineering-blog.netlify.app";
-export const SITE_DESCRIPTION = "Welcome to our Engineering blog!";
+export const BRAND = "Louie Fitzpatrick";
+export const SITE_TITLE = "Louie Fitzpatrick Software Engineering Blog";
+export const SITE_URL = "https://louie-fitz.com";
+export const SITE_DESCRIPTION = "A software engineering portfolio and blog by Louie Fitzpatrick.";
 export const PAGINATION_POSTS_PER_PAGE = 5;
