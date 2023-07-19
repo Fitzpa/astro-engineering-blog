ed693773e555325fb8c9e496872fd72c6798ea8f
Louie Fitzpatrick <fitz4492@gmail.com>
2023-07-19 14:01:09 -0500

    adding vercel astro adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import solidJs from "@astrojs/solid-js";
 import tailwind from "@astrojs/tailwind";
+import vercel from '@astrojs/vercel/serverless';
 import { SITE_URL } from "./src/consts.ts";
 
 // https://astro.build/config
@@ -25,4 +26,6 @@ export default defineConfig({
     solidJs(),
     tailwind(),
   ],
+  output: 'server',
+  adapter: vercel(),
 });
