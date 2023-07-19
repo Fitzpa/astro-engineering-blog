b4783908554e7714088be9e20a57d397f7f3fbe6
Louie Fitzpatrick <fitz4492@gmail.com>
2023-07-19 12:55:05 -0500

    TechStack and WorkSections added

--- src/data/workSectionData.ts
+++ src/data/workSectionData.ts
@@ -2,36 +2,36 @@ export const workSectionData = [
   {
     name: "When I Work",
     role: "Frontend Software Engineer",
-    path: "when-i-work.webp",
+    path: "src/assets/images/previous-work/when-i-work.webp",
   },
   {
     name: "SPACE150",
     role: "Frontend Software Engineer",
-    path: "space.webp",
+    path: "src/assets/images/previous-work/space.webp",
   },
   {
     name: "Red Wing Shoes",
     role: "Frontend Software Engineer",
-    path: "red-wing.webp",
+    path: "src/assets/images/previous-work/red-wing.webp",
   },
   {
     name: "Phillips Distilling Company",
     role: "Frontend Software Engineer",
-    path: "cubist.webp",
+    path: "src/assets/images/previous-work/cubist.webp",
   },
   {
     name: "Phillips Distilling Company",
     role: "Frontend Software Engineer",
-    path: "uv.webp",
+    path: "src/assets/images/previous-work/uv.webp",
   },
   {
     name: "WP Engine",
     role: "Frontend Software Engineer",
-    path: "wp-engine.webp",
+    path: "src/assets/images/previous-work/wp-engine.webp",
   },
   {
     name: "Nike X Tom Sachs",
     role: "Fullstack Software Engineer",
-    path: "nikecraft.webp",
+    path: "src/assets/images/previous-work/nikecraft.webp",
   },
 ];
