d0f3a1b77d4b1cfeb421251d65e3cdedbab9e021
Louie Fitzpatrick <fitz4492@gmail.com>
2023-07-18 23:50:08 -0500

    tailwind updates

---
+++ .prettier.config.cjs
@@ -0,0 +1,18 @@
+
+module.exports = {
+  plugins: [
+    require.resolve("prettier-plugin-tailwindcss"),
+    require.resolve("prettier-plugin-astro")
+  ],
+  tailwindConfig: "./tailwind.config.cjs",
+  overrides: [
+    {
+      files: '*.astro',
+      options: {
+        parser: 'astro',
+      },
+    },
+  ],
+}
+
+// export default config
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,31 +1,54 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
     extend: {
       fontFamily: {
-        berka: ["BerkaLight", "sans-serif"],
+        berka: ['BerkaLight', 'sans-serif'],
+        sans: ['BerkaLight', 'sans-serif'],
+        serif: ['BerkaLight', 'sans-serif'],
+        mono: ['BerkaLight', 'sans-serif'],
+        display: ['BerkaLight', 'sans-serif'],
+        body: ['BerkaLight', 'sans-serif'],
       },
       colors: {
-        header: "#000",
-        "header-foreground": "#fff",
-        link: "#2563EB",
+        header: '#000',
+        'header-foreground': '#fff',
+        link: '#2563EB',
         primary: {
-          500: "#2563EB",
-          600: "#1D4ED8",
+          500: '#2563EB',
+          600: '#1D4ED8',
         },
-        "primary-foreground": "#fff",
+        'primary-foreground': '#fff',
       },
       typography: (theme) => ({
         DEFAULT: {
           css: {
             a: {
-              color: theme("colors.link"),
+              color: theme('colors.link'),
             },
+
           },
         },
       }),
     },
   },
-  plugins: [require("@tailwindcss/typography"), require("daisyui")],
-};
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          "primary": "#4728D3",
+          "secondary": "#663BC2",
+          "accent": "#1dcdbc",
+          "neutral": "#2b3440",
+          "base-100": "#ffffff",
+          "info": "#3abff8",
+          "success": "#36d399",
+          "warning": "#fbbd23",
+          "error": "#f87272",
+        },
+      },
+    ],
+  },
+  plugins: [require('@tailwindcss/typography'), require('daisyui')],
+}
